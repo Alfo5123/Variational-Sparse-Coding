1f1791c7cf283b930066651430f35ae9f4ea5055
Robert-Alonso <robert.alonso.ac@gmail.com>
2018-11-01 13:33:10 -0500

    Add setup files

---
+++ folder-setup.py
@@ -0,0 +1,7 @@
+import os
+
+os.makedirs('data/mnist', exist_ok=True)
+os.makedirs('data/fashion-mnist', exist_ok=True)
+os.makedirs('data/celeba', exist_ok=True)
+os.makedirs('notebooks', exist_ok=True)
+os.makedirs('src', exist_ok=True)
