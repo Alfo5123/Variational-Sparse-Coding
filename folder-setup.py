ada6ba0028055e49904682a54213f9f5a1797964
Alfo5123 <alfredo.delafuente.b@gmail.com>
2018-11-02 00:23:37 +0300

    Update setup file to download datasets

--- folder-setup.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.makedirs('data/mnist', exist_ok=True)
-os.makedirs('data/fashion-mnist', exist_ok=True)
-os.makedirs('data/celeba', exist_ok=True)
-os.makedirs('notebooks', exist_ok=True)
-os.makedirs('src', exist_ok=True)
